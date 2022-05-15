26e05b6b90ff41dc8d481d3527dcccb36c9efd64
Kym Reilly <kymreilly@hotmail.com>
2022-05-15 21:02:25 +0930

    fixed function for naming convention, implemented tests for Intern.test.js. All passed

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,23 @@
 const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+  describe("Initialization", () => {
+    it("should return Intern school", () => {
+      const schoolVal = "AdelUni";
+      const intern = new Intern("Conrad", 15, "Hermes@gmail.com", schoolVal);
+      expect(intern.school).toBe(schoolVal);
+    });
+
+    it("should return Intern string from getRole function", () => {
+      const roleVal = "Intern";
+      const intern = new Intern("Conrad", 15, "Hermes@gmail.com", "AdelUni");
+      expect(intern.getRole()).toBe(roleVal);
+    });
+
+    it("should return username for GitHub from getGithub function", () => {
+      const schoolVal = "AdelUni";
+      const intern = new Intern("Conrad", 15, "Hermes@gmail.com", schoolVal);
+      expect(intern.getSchool()).toBe(schoolVal);
+    });
+  });
+});
