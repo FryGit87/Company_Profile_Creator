df07ca39c72a90ceb902c635e68930d9f4b51b00
Kym Reilly <kymreilly@hotmail.com>
2022-05-16 11:44:43 +0930

    Questions for manager implemented. Run function to start the queries

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const staff = [];
+
+function run() {
+  getManagerInfo();
+}
+
+function getManagerInfo() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team manager's office number?",
+    },
+  ]);
+}
